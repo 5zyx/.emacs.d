beab646bf6faabe27468122c0784f8fc400719cb
yorick <yorick.yuan@verystar.cn>
2019-12-12 10:34:20 +0800

    custom-post

--- custom-post.el
+++ custom-post.el
@@ -1,18 +1,116 @@
 (use-package evil
-  :init
-  (evil-mode 1)
-  ;;(lsp-ui-doc-enable nil)
-  )
+  :init (evil-mode 1))
 
-;;; custom.el ends here
+(tool-bar-mode 1)
+(menu-bar-mode 1)
+(scroll-bar-mode 1)
+
+;; How to detect the project root.
+;; The default priority of detection is `ffip' > `projectile' > `project'.
+;; nil means to use `default-directory'.
+;; The project management packages have some issues on detecting project root.
+;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
+;; to hanle sub-projects.
+;; You can specify one if you encounter the issue.
+;;(setq doom-modeline-project-detection 'project)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(go-test-verbose t)
+ ;;'(godef-command "godef")
  '(lsp-ui-doc-position (quote top)))
 
+;;
 (add-hook 'go-mode-hook (lambda()
-                          (local-set-key (kbd "C-c C-j") 'lsp-find-definition) ))
-;;end
+                          (local-set-key (kbd "C-c C-j") 'lsp-find-definition)))
+
+(add-hook 'php-mode-hook (lambda()
+                          (local-set-key (kbd "C-c C-j") 'lsp-find-definition)))
+
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code."
+  t)
+
+(require 'cmuscheme)
+(setq scheme-program-name "scheme")
+
+(defun scheme-proc ()
+  "Return the current Scheme process, starting one if necessary."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name)))
+  (or (scheme-get-process)
+      (error "No current process. See variable `scheme-buffer'")))
+
+(defun scheme-split-window ()
+  (cond
+   ((= 1 (count-windows))
+    (delete-other-windows)
+    (split-window-vertically (floor (* 0.68 (window-height))))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window 1))
+   ((not (find "*scheme*"
+               (mapcar (lambda (w) (buffer-name (window-buffer w)))
+                       (window-list))
+               :test 'equal))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window -1))))
+
+(defun scheme-send-last-sexp-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-last-sexp))
+
+(defun scheme-send-definition-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-definition))
+
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (paredit-mode 1)
+            (define-key scheme-mode-map (kbd "<f5>") 'scheme-send-last-sexp-split-window)
+            (define-key scheme-mode-map (kbd "<f6>") 'scheme-send-definition-split-window)))
+
+
+(setq geiser-active-implementations '(chez))   ;;; Otherwise, use C+c C+s to select
+
+;;同步文件
+;;;###autoload
+(defun ora-dired-rsync (dest)
+  (interactive
+   (list
+    (expand-file-name
+     (read-file-name
+      "Rsync to:"
+      (dired-dwim-target-directory)))))
+  ;; store all selected files into "files" list
+  (let ((files (dired-get-marked-files
+                nil current-prefix-arg))
+        ;; the rsync command
+        (tmtxt/rsync-command
+         "rsync -arvz --progress "))
+    ;; add all selected file names as arguments
+    ;; to the rsync command
+    (dolist (file files)
+      (setq tmtxt/rsync-command
+            (concat tmtxt/rsync-command
+                    (shell-quote-argument file)
+                    " ")))
+    ;; append the destination
+    (setq tmtxt/rsync-command
+          (concat tmtxt/rsync-command
+                  (shell-quote-argument dest)))
+    ;; run the async shell command
+    (async-shell-command tmtxt/rsync-command "*rsync*")
+    ;; finally, switch to that window
+    (other-window 1)))
+
+(define-key dired-mode-map "Y" 'ora-dired-rsync)
