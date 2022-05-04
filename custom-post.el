23c0b3d0abe9870b4ca3878121f45e4519649f89
wow-yorick <wowyorick@126.com>
2022-05-04 12:47:44 +0800

    resolve

--- custom-post.el
+++ custom-post.el
@@ -5,3 +5,24 @@
 (tool-bar-mode 1)
 (menu-bar-mode 1)
 (scroll-bar-mode 1)
+
+(setq org-confirm-babel-evaluate nil
+      org-src-fontify-natively t
+      org-src-tab-acts-natively t)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell         . t)
+   (js         . t)
+   (emacs-lisp . t)
+   (perl       . t)
+   (clojure    . t)
+   (python     . t)
+   (ruby       . t)
+   (dot        . t)
+   (go        . t)
+   (css        . t)
+   (calc        . t)
+   (sql        . t)
+   (sqlite        . t)
+   (plantuml   . t)))
