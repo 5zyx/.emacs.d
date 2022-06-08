c09d1d4e9d275b55ae83ec42c3bc9e1339c3a9bd
wow-yorick <wowyorick@126.com>
2022-06-08 22:15:30 +0800

    test

---
+++ custom.el
@@ -0,0 +1,152 @@
+;;; custom.el --- user customization file    -*- no-byte-compile: t -*-
+;;; Commentary:
+;;;       Add or change the configurations in custom.el, then restart Emacs.
+;;;       Put your own configurations in custom-post.el to override default configurations.
+;;; Code:
+
+;; (setq centaur-logo nil)                        ; Logo file or nil (official logo)
+;; (setq centaur-full-name "user name")           ; User full name
+;; (setq centaur-mail-address "user@email.com")   ; Email address
+(setq centaur-proxy "127.0.0.1:7890")          ; Network proxy
+;; (setq centaur-server nil)                      ; Enable `server-mode' or not: t or nil
+;; (setq centaur-icon nil)                        ; Display icons or not: t or nil
+(setq centaur-package-archives 'melpa)   ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
+;; (setq centaur-theme 'light)                    ; Color theme: auto, random, default, classic, colorful, dark, light, day or night
+;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
+;; (setq centaur-lsp 'eglot)                      ; Set LSP client: lsp-mode, eglot or nil
+;; (setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode)) ; Ignore format on save for some languages
+;; (setq centaur-chinese-calendar t)              ; Use Chinese calendar or not: t or nil
+(setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications
+(setq centaur-prettify-org-symbols-alist nil)  ; Alist of symbol prettifications for `org-mode'
+;; (setq centaur-benchmark-init t)                ; Enable initialization benchmark or not: t or nil
+
+;; For Emacs devel
+;; (setq package-user-dir (locate-user-emacs-file (format "elpa-%s" emacs-major-version)))
+;; (setq desktop-base-file-name (format ".emacs-%s.desktop" emacs-major-version))
+;; (setq desktop-base-lock-name (format ".emacs-%s.desktop.lock" emacs-major-version))
+
+;; Fonts
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code"
+                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return (set-face-attribute 'default nil
+                                      :font font
+                                      :height (cond (sys/mac-x-p 130)
+                                                    (sys/win32p 110)
+                                                    (t 100))))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("Symbola" "Apple Symbols" "Symbol" "icons-in-terminal")
+           when (font-installed-p font)
+           return (set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+
+;; Mail
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;       smtpmail-auth-credentials '(("smtp.gmail.com" 587
+;;                                    user-mail-address nil))
+;;       smtpmail-default-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-service 587)
+
+;; Calendar
+;; Set location , then press `S' can show the time of sunrise and sunset
+;; (setq calendar-location-name "Chengdu"
+;;       calendar-latitude 30.67
+;;       calendar-longitude 104.07)
+
+;; Misc.
+;; (setq confirm-kill-emacs 'y-or-n-p)
+
+;; Display on the specified monitor
+;; (when (and (> (length (display-monitor-attributes-list)) 1)
+;;            (> (display-pixel-width) 1920))
+;;   (set-frame-parameter nil 'left 1920))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/yorick/Desktop/inspiration/draft.org" "/Users/yorick/Desktop/inspiration/pipe.org"))
+ '(org-confirm-babel-evaluate nil)
+ '(org-export-with-sub-superscripts '{})
+ '(org-hugo-base-dir "~/workspace/blog/")
+ '(org-plantuml-jar-path "~/.emacs.d/tools/plantuml-1.2022.4.jar")
+ '(org-use-sub-superscripts '{})
+ '(plantuml-jar-args nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
+ '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 1.0))))
+ '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
+ '(cfrs-border-color ((t (:background "#5B6268"))))
+ '(company-box-selection ((t (:inherit company-tooltip :weight semibold :extend t))) t)
+ '(company-tooltip-annotation ((t (:inherit completions-annotations :foreground nil))))
+ '(dashboard-heading ((t (:inherit (font-lock-string-face bold)))))
+ '(diff-hl-change ((t (:foreground "#51afef" :background nil))))
+ '(diff-hl-delete ((t (:inherit diff-removed :background nil))))
+ '(diff-hl-insert ((t (:inherit diff-added :background nil))))
+ '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
+ '(flycheck-posframe-background-face ((t (:inherit tooltip))))
+ '(flycheck-posframe-border-face ((t (:inherit posframe-border))))
+ '(flycheck-posframe-face ((t (:foreground "#98be65"))))
+ '(flycheck-posframe-info-face ((t (:foreground "#98be65"))))
+ '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(hl-todo ((t (:inherit default :height 0.9 :width condensed :weight bold :underline nil :inverse-video t))))
+ '(ivy-current-match ((t (:inherit region :distant-foreground nil :background nil))))
+ '(ivy-minibuffer-match-face-1 ((t (:foreground "#83898d"))))
+ '(ivy-minibuffer-match-face-2 ((t (:distant-foreground nil :background nil))))
+ '(ivy-minibuffer-match-face-3 ((t (:distant-foreground nil :background nil))))
+ '(ivy-minibuffer-match-face-4 ((t (:distant-foreground nil :background nil))))
+ '(ivy-posframe ((t (:inherit tooltip))))
+ '(ivy-posframe-border ((t (:inherit posframe-border))))
+ '(lsp-headerline-breadcrumb-path-error-face ((t :underline (:style wave :color "#ff6c6b") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-hint-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-info-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-warning-face ((t :underline (:style wave :color "#ECBE7B") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-symbols-error-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ff6c6b"))))
+ '(lsp-headerline-breadcrumb-symbols-hint-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
+ '(lsp-headerline-breadcrumb-symbols-info-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
+ '(lsp-headerline-breadcrumb-symbols-warning-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ECBE7B"))))
+ '(lsp-ui-sideline-code-action ((t (:inherit warning))))
+ '(macrostep-expansion-highlight-face ((t (:inherit tooltip :extend t))))
+ '(org-ellipsis ((t (:foreground nil))))
+ '(org-pomodoro-mode-line ((t (:inherit warning))))
+ '(org-pomodoro-mode-line-break ((t (:inherit success))))
+ '(org-pomodoro-mode-line-overtime ((t (:inherit error))))
+ '(paradox-archive-face ((t (:inherit font-lock-doc-face))))
+ '(paradox-description-face ((t (:inherit completions-annotations))))
+ '(pulse-highlight-face ((t (:inherit region))))
+ '(pulse-highlight-start-face ((t (:inherit region))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold)))))
+ '(transient-posframe ((t (:inherit tooltip))))
+ '(transient-posframe-border ((t (:inherit posframe-border))))
+ '(which-key-posframe ((t (:inherit tooltip))))
+ '(which-key-posframe-border ((t (:inherit posframe-border))))
+ '(ztreep-arrow-face ((t (:inherit font-lock-comment-face))))
+ '(ztreep-diff-header-face ((t (:inherit (diff-header bold)))))
+ '(ztreep-diff-header-small-face ((t (:inherit diff-file-header))))
+ '(ztreep-diff-model-add-face ((t (:inherit diff-nonexistent))))
+ '(ztreep-diff-model-diff-face ((t (:inherit diff-removed))))
+ '(ztreep-diff-model-ignored-face ((t (:inherit font-lock-doc-face :strike-through t))))
+ '(ztreep-diff-model-normal-face ((t (:inherit font-lock-doc-face))))
+ '(ztreep-expand-sign-face ((t (:inherit font-lock-function-name-face))))
+ '(ztreep-header-face ((t (:inherit diff-header))))
+ '(ztreep-leaf-face ((t (:inherit diff-index))))
+ '(ztreep-node-face ((t (:inherit font-lock-variable-name-face)))))
+
+;;; custom.el ends here
