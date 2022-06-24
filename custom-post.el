9587dd79b8489b89127a0e0a020f70efb6ae84d2
wow-yorick <wowyorick@126.com>
2022-06-24 21:37:21 +0800

    test

--- custom-post.el
+++ custom-post.el
@@ -12,3 +12,12 @@
 
 (add-hook 'org-insert-heading-hook
           (lambda () (org-set-property "CREATED" (format-time-string "%Y/%m/%d %H:%M"))))
+
+(defun endless/org-ispell ()
+  "Configure `ispell-skip-region-alist' for `org-mode'."
+  (make-local-variable 'ispell-skip-region-alist)
+  (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
+  (add-to-list 'ispell-skip-region-alist '("~" "~"))
+  (add-to-list 'ispell-skip-region-alist '("=" "="))
+  (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC" . "^#\\+END_SRC")))
+(add-hook 'org-mode-hook #'endless/org-ispell)
