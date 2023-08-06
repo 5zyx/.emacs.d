09b3602dc8ad6fdabebe9809ced09ada61bf6b2e
wow-yorick <wowyorick@126.com>
2023-08-06 17:43:56 +0800

    feat: emacs

---
+++ tabsession.el
@@ -0,0 +1,8 @@
+;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
+;; -------------------------------------------------------------------------
+;; Tabspaces Session File for Emacs
+;; -------------------------------------------------------------------------
+;; Created Sat Aug  5 14:53:42 2023
+
+;; Tabs and buffers:
+(setq tabspaces--session-list '((nil . "*scratch*")))
