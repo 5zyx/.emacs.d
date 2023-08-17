3caf9eda4d145a55e8ad9fd14dc37244f423c877
yorick <yorick.yuan@verystar.cn>
2023-08-17 09:56:40 +0800

    feat: emacs

--- tabsession.el
+++ tabsession.el
@@ -2,7 +2,7 @@
 ;; -------------------------------------------------------------------------
 ;; Tabspaces Session File for Emacs
 ;; -------------------------------------------------------------------------
-;; Created Wed Aug 16 16:05:00 2023
+;; Created Wed Aug 16 18:30:43 2023
 
 ;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/yudong/org/idea.org" "/Users/yudong/org/work-flow.org" "/Users/yudong/org/note.org" "/Users/yudong/.emacs.d/custom.el") . "*scratch*")))
+(setq tabspaces--session-list '((("/Users/yudong/workspace/wow-yorick/blog/content/posts/计算机程序构造和解释摘抄.md" "/Users/yudong/.emacs.d/custom.el" "/Users/yudong/org/note.org" "/Users/yudong/org/work-flow.org" "/Users/yudong/org/idea.org") . "*scratch*")))
