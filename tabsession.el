2dfbc59a7e6e095a413a74324ff76eb835498654
wow-yorick <wowyorick@126.com>
2023-08-19 18:22:47 +0800

    feat

--- tabsession.el
+++ tabsession.el
@@ -2,7 +2,6 @@
 ;; -------------------------------------------------------------------------
 ;; Tabspaces Session File for Emacs
 ;; -------------------------------------------------------------------------
-;; Created Sat Aug 19 18:20:33 2023
+;; Created Sat Aug 19 18:22:12 2023
 
 ;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/yudong/.emacs.d/custom.el" "/Users/yudong/org/note.org" "/Users/yudong/org/work-flow.org" "/Users/yudong/org/idea.org" "/Users/yudong/workspace/wow-yorick/blog/content/posts/计算机程序构造和解释摘抄.md") . "*scratch*")))
