b35fd3baafccc0e311902117b9dd509992b60d78
yorick <yorick.yuan@verystar.cn>
2023-08-22 18:22:43 +0800

    feat

--- tabsession.el
+++ tabsession.el
@@ -2,7 +2,7 @@
 ;; -------------------------------------------------------------------------
 ;; Tabspaces Session File for Emacs
 ;; -------------------------------------------------------------------------
-;; Created Mon Aug 21 17:45:41 2023
+;; Created Tue Aug 22 18:19:51 2023
 
 ;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/yudong/.emacs.d/.mc-lists.el" "/Users/yudong/.emacs.d/banner.txt" "/Users/yudong/.emacs.d/lisp/init-ui.el" "/Users/yudong/.emacs.d/lisp/init-base.el" "/Users/yudong/.emacs.d/custom.el" "/Users/yudong/org/work-flow.org") . "*scratch*")))
+(setq tabspaces--session-list '((("/Users/yudong/workspace/wow-yorick/blog/content/posts/计算机程序构造和解释摘抄.md" "/Users/yudong/org/work-flow.org" "/Users/yudong/.emacs.d/custom.el" "/Users/yudong/.emacs.d/lisp/init-base.el" "/Users/yudong/.emacs.d/lisp/init-ui.el" "/Users/yudong/.emacs.d/banner.txt" "/Users/yudong/.emacs.d/.mc-lists.el") . "*scratch*")))
