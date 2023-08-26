993bdd217cf870c64a7914c142a3c61637fdf442
wow-yorick <wowyorick@126.com>
2023-08-26 11:48:07 +0800

    feat: emacs

--- tabsession.el
+++ tabsession.el
@@ -2,7 +2,7 @@
 ;; -------------------------------------------------------------------------
 ;; Tabspaces Session File for Emacs
 ;; -------------------------------------------------------------------------
-;; Created Wed Aug 23 11:52:27 2023
+;; Created Thu Aug 24 19:43:44 2023
 
 ;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/yudong/workspace/wow-yorick/blog/content/posts/计算机程序构造和解释摘抄.md" "/Users/yudong/.emacs.d/.mc-lists.el" "/Users/yudong/.emacs.d/banner.txt" "/Users/yudong/.emacs.d/lisp/init-ui.el" "/Users/yudong/.emacs.d/lisp/init-base.el" "/Users/yudong/.emacs.d/custom.el" "/Users/yudong/org/work-flow.org") . "*scratch*")))
+(setq tabspaces--session-list '((("/Users/yudong/org/work-flow.org" "/Users/yudong/.emacs.d/custom.el" "/Users/yudong/.emacs.d/lisp/init-base.el" "/Users/yudong/.emacs.d/lisp/init-ui.el" "/Users/yudong/.emacs.d/banner.txt" "/Users/yudong/.emacs.d/.mc-lists.el" "/Users/yudong/workspace/wow-yorick/blog/content/posts/计算机程序构造和解释摘抄.md") . "*scratch*")))
