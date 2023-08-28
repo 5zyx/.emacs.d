28dea60f5a8cf6c6dc391230bf8dd9de5aa23707
yorick <yorick.yuan@verystar.cn>
2023-08-28 16:41:27 +0800

    feat

--- tabsession.el
+++
@@ -1,8 +0,0 @@
-;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
-;; -------------------------------------------------------------------------
-;; Tabspaces Session File for Emacs
-;; -------------------------------------------------------------------------
-;; Created Mon Aug 28 09:38:16 2023
-
-;; Tabs and buffers:
-(setq tabspaces--session-list '((nil . "*scratch*")))
