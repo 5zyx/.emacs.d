1a4f5584e7b63655dc42897c928c974f5011451e
yorick <yorick.yuan@verystar.cn>
2025-11-28 11:40:11 +0800

    配置

---
+++ tools/go-tools-update.sh
@@ -0,0 +1,15 @@
+ go get -u -v github.com/mdempsky/gocode
+ go get -u -v github.com/rogpeppe/godef
+ go get -u -v golang.org/x/tools/cmd/gopls
+ go get -u -v golang.org/x/tools/cmd/goimports
+ go get -u -v golang.org/x/tools/cmd/gorename
+ go get -u -v golang.org/x/tools/cmd/gotype
+ go get -u -v golang.org/x/tools/cmd/godoc
+ go get -u -v github.com/go-delve/delve/cmd/dlv
+ go get -u -v github.com/josharian/impl
+ go get -u -v github.com/cweill/gotests/...
+ go get -u -v github.com/fatih/gomodifytags
+ go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+#go get github.com/uudashr/gopkgs/cmd/gopkgs@latest
+ #go get -u -v onnef.co/go/tools/...
+go get -u github.com/haya14busa/gopkgs/cmd/gopkgs
